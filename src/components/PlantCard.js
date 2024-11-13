65ff5e38edad028926b8da7a8bcded3a1aea3beb
Ian <ian.mosonik@student.moringaschool.com>
2024-11-13 12:39:42 +0300

    update

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,33 +1,34 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-function PlantCard({setPlant ,plants}) {
-  useEffect(()=>{
+function PlantCard({setPlant, plants}) {
+  const [stockStatus, setStockStatus] = useState({});
 
+  useEffect(() => {
     fetch("http://localhost:6001/plants")
     .then(response => response.json())
     .then(data => setPlant(data))
-  },[setPlant])
+  }, [setPlant]);
 
-  
-  
+  const handleStockClick = (id) => {
+    setStockStatus(prev => ({
+      ...prev,
+      [id]: !prev[id]
+    }));
+  };
 
-  
-  
   return (
-    plants.map((plant)=>(
-      
-
-    <li key={plant.id} className="card" data-testid="plant-item">
-      <img src={plant.image} alt={plant.name} />
-      <h4>{plant.name}</h4>
-      <p>Price: {plant.price}</p>
-      {plant.id ? (
-        <button  className="primary">In Stock</button>
-      ) : (
-        <button >Out of Stock</button>
-      )}
-    </li>
+    plants.map((plant) => (
+      <li key={plant.id} className="card" data-testid="plant-item">
+        <img src={plant.image} alt={plant.name} />
+        <h4>{plant.name}</h4>
+        <p>Price: {plant.price}</p>
+        <button 
+          className={stockStatus[plant.id] ? "" : "primary"}
+          onClick={() => handleStockClick(plant.id)}
+        >
+          {stockStatus[plant.id] ? "Out of Stock" : "In Stock"}
+        </button>
+      </li>
     ))
   );
 }
